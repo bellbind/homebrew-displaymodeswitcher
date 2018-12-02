9437f71000fe39edaaef7645586862e427fc9c4b
bellbind <bellbind@gmail.com>
2018-12-02 19:29:48 +0900

    fixed with brew audit

--- Formula/displaymodeswitcher.rb
+++ Formula/displaymodeswitcher.rb
@@ -1,22 +1,28 @@
 class Displaymodeswitcher < Formula
-  desc "Tiny status bar utility for switching display resolution on macos"
+  desc "Status bar utility for switching display resolution"
   homepage "https://github.com/bellbind/DisplayModeSwitcher/"
+  
   url "https://github.com/bellbind/DisplayModeSwitcher/archive/1.1.0.tar.gz"
   sha256 "8177128915492cf40d1c723b644017716917cd2a8d02484386ffcd1b6758d20b"
+
+  head "https://github.com/bellbind/DisplayModeSwitcher.git"
 
   devel do
     # [Hidden API version: brew install --devel --no-sandbox displaymodeswitcher]
     url "https://github.com/bellbind/DisplayModeSwitcher/archive/1.1.0h.tar.gz"
+    version "1.1.0h"
     sha256 "29ed7e5731a34b2d98a65585b8e82cdcec351fbf259309d6acb68f4681029caa"
   end
   
-  depends_on :xcode => :build
   depends_on "imagemagick" => :build
+  depends_on :xcode => :build
 
   def install
+    ohai ""
     ohai "[NOTICE] For generating app icon, qlmanage command required to run outside of sandbox:"
     ohai "$ brew uninstall displaymodeswitcher"
     ohai "$ brew install --no-sandbox displaymodeswitcher"
+    ohai ""
     system "./makeappbundle.sh"
     prefix.install "DisplayModeSwitcher.app"
     bin.install_symlink prefix/"DisplayModeSwitcher.app/Contents/MacOS/DisplayModeSwitcher"
